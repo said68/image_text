f128e85feb69e2a739746cead014ef063a4ba5ea
said68 <35385531+said68@users.noreply.github.com>
2024-05-30 12:55:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import streamlit as st
 from PIL import Image
-import pytesseract
+import io
+from google.cloud import vision
+from google.oauth2 import service_account
+
+# Remplacez 'path_to_your_service_account.json' par le chemin de votre fichier de clé JSON
+credentials = service_account.Credentials.from_service_account_file('path_to_your_service_account.json')
+client = vision.ImageAnnotatorClient(credentials=credentials)
 
 def extract_text_from_image(image):
-    text = pytesseract.image_to_string(image)
-    return text
+    content = io.BytesIO()
+    image.save(content, format='PNG')
+    image_content = content.getvalue()
+    
+    image = vision.Image(content=image_content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    
+    if texts:
+        return texts[0].description
+    else:
+        return "Aucun texte détecté"
 
-st.title('Application OCR avec Streamlit')
+st.title('Application conversion image-text')
 
 uploaded_file = st.file_uploader("Choisissez une image...", type=["jpg", "jpeg", "png"])
 
