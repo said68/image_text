686ef3d260bb4eacf1b629a2a225ddb62f266ac1
said68 <35385531+said68@users.noreply.github.com>
2024-05-30 11:04:39 -0400

    Add files via upload

---
+++ image_to_text.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+import pytesseract
+
+def extract_text_from_image(image):
+    text = pytesseract.image_to_string(image)
+    return text
+
+st.title('Application OCR avec Streamlit')
+
+uploaded_file = st.file_uploader("Choisissez une image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Image téléchargée', use_column_width=True)
+    st.write("")
+
+    if st.button('Extraire'):
+        with st.spinner("Conversion en cours..."):
+            text = extract_text_from_image(image)
+        st.write("Texte extrait :")
+        st.write(text)
+
+    if st.button('Initialiser'):
+        st.experimental_rerun()
