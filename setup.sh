b7f99d6c401f582143ff11873fcb8b4bf06d2e25
said68 <35385531+said68@users.noreply.github.com>
2024-05-30 12:34:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-fra
+apt-get update
+apt-get install -y tesseract-ocr tesseract-ocr-fra
+streamlit run main.py --server.port 8080
